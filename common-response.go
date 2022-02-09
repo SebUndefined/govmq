ae78f2a0660b230d93b30149f28a9f30f6d68f90
Sébastien TACHIER <sebastien.tachier@inforsud.com>
2022-02-09 10:06:40 +0100

    Change structure

---
+++ common-response.go
@@ -0,0 +1,25 @@
+package govmq
+
+type Response interface {
+}
+
+type OKResponse struct {
+	Result string `json:"result"`
+}
+
+// ErrorResponse an error response that satisfy VMQ Broker
+type ErrorResponse struct {
+	Result struct {
+		Error string `json:"error"`
+	} `json:"result"`
+}
+
+func NewErrorResponse(result struct {
+	Error string `json:"error"`
+}) *ErrorResponse {
+	return &ErrorResponse{Result: result}
+}
+
+type Builder interface {
+	Build() Response
+}
