a08706ffdb23f3ecfb3d00c85e640e5bcbdba175
Sébastien TACHIER <sebastien.tachier@inforsud.com>
2022-02-09 17:07:01 +0100

    Add modifier

--- session_lifecycle_modifier.go
+++ session_lifecycle_modifier.go
@@ -1,22 +1,22 @@
 package govmq
 
 type AuthOnRegisterModifier struct {
-	MountPoint               *string     `json:"mountpoint"`
-	MaxMessageSize           *int        `json:"max_message_size"`
-	SharedSubscriptionPolicy interface{} `json:"shared_subscription_policy"`
-	MaxOnlineMessages        *int        `json:"max_online_messages "`
-	MaxOfflineMessages       *int        `json:"max_offline_messages "`
+	MountPoint               string      `json:"mountpoint,omitempty"`
+	MaxMessageSize           int         `json:"max_message_size,omitempty"`
+	SharedSubscriptionPolicy interface{} `json:"shared_subscription_policy,omitempty"`
+	MaxOnlineMessages        int         `json:"max_online_messages,omitempty"`
+	MaxOfflineMessages       int         `json:"max_offline_messages,omitempty"`
 }
 
 type AuthOnRegisterM3Modifier struct {
 	AuthOnRegisterModifier `json:",inline"`
-	CleanSession           *bool `json:"clean_session"`
+	CleanSession           bool `json:"clean_session,omitempty"`
 }
 
 type AuthOnRegisterM5Modifier struct {
 	AuthOnRegisterModifier `json:",inline"`
-	CleanStart             *bool `json:"clean_start"`
-	Properties             *struct {
-		SessionExpiryInterval *int `json:"p_session_expiry_interval"`
+	CleanStart             bool `json:"clean_start,omitempty"`
+	Properties             struct {
+		SessionExpiryInterval int `json:"p_session_expiry_interval,omitempty"`
 	} `json:"properties"`
 }
