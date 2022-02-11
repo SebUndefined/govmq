25657e7925e2a7bc56914b71cbca9cc795af8d36
Sébastien TACHIER <sebastien.tachier@inforsud.com>
2022-02-11 16:32:41 +0100

    Add doc and change field with pointer

--- session_lifecycle_modifier_builder.go
+++ session_lifecycle_modifier_builder.go
@@ -1,14 +1,31 @@
 package govmq
 
-type BuilderOption func(r *AuthOnRegisterModifier)
-
 type AuthOnRegisterModifierBuilder struct {
-	actions []BuilderOption
+	actions []func(r *AuthOnRegisterModifier)
 }
 
 func (rb *AuthOnRegisterModifierBuilder) WithMountpoint(mountpoint string) *AuthOnRegisterModifierBuilder {
 	rb.actions = append(rb.actions, func(r *AuthOnRegisterModifier) {
 		r.MountPoint = &mountpoint
+	})
+	return rb
+}
+
+type AuthOnRegisterM3ModifierBuilder struct {
+	actions []func(r *AuthOnRegisterM3Modifier)
+}
+
+func (rb *AuthOnRegisterM3ModifierBuilder) WithBase(f func(*AuthOnRegisterModifierBuilder) *AuthOnRegisterModifierBuilder) *AuthOnRegisterM3ModifierBuilder {
+
+	rb.actions = append(rb.actions, func(r *AuthOnRegisterM3Modifier) {
+		r.CleanSession = &c
+	})
+	return rb
+}
+
+func (rb *AuthOnRegisterM3ModifierBuilder) WithCleanSession(c bool) *AuthOnRegisterM3ModifierBuilder {
+	rb.actions = append(rb.actions, func(r *AuthOnRegisterM3Modifier) {
+		r.CleanSession = &c
 	})
 	return rb
 }
